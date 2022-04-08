d39988fd2ce2798263fa5e93cd4350f50fd1ea5f
GiannisArk <giannis.ark99@gmail.com>
2022-04-08 15:27:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,9 +25,9 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Codelab';
+  const title = 'Giannis';
   const options = {
-    body: 'lalala',
+    body: '<b>testme</b>',
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };
