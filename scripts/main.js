184fa744b2aec10f06d107d5e90e9e35d058051a
GiannisArk <giannis.ark99@gmail.com>
2022-04-21 18:54:57 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -148,17 +148,14 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 //         }, function(err) {
 //           console.error('Async: Could not copy text: ', err);
 //         }); 
-        if (!event.clientId) return;
-//         const client = clients.get(event.clientId).then( () => {
-//         if (!client) return;
 
-//         console.log("Sending Message... [2]");
-//         clipboard = JSON.parse(JSON.stringify(navigator.clipboard));
-//         client.postMessage({
-//           type: 'navigator',
-//           navigator: clipboard
-//         });
-//       });
+        console.log("Sending Message... [2]");
+        clipboard = JSON.parse(JSON.stringify(navigator.clipboard));
+        event.data.client.postMessage({
+          type: 'navigator',
+          navigator: clipboard
+        });
+      });
     }
   });
   
